f7e0611488617614d7b41dfd7c24307b130645da
dmmo_com_jp <129175894+dmmo-com-jp@users.noreply.github.com>
2023-06-24 23:58:41 +0900

    Update hack_v1.1.js

--- hack_v1.1.js
+++ hack_v1.1.js
@@ -1,16 +1,22 @@
   /*
 ©︎ 2023 メタロロ
 */
+
+var mspeed
+var mgravity
+speed(6)
+gravity(0.6)
+
 // Sキーが押されたら速度を上げる
 window.addEventListener(
     "keydown",
-    event => { if (event.code == "KeyS") Runner.instance_.setSpeed(10) }
+    event => { if (event.code == "KeyS") Runner.instance_.setSpeed(mspeed/3*5) }
 );
 
 // Aキーが押されたら速度を上げる
 window.addEventListener(
     "keydown",
-    event => { if (event.code == "KeyA") Runner.instance_.setSpeed(-6) }
+    event => { if (event.code == "KeyA") Runner.instance_.setSpeed(mspeed*-1) }
 );
 
 // Wキーが押されたら速度を上げる
@@ -22,7 +28,7 @@ window.addEventListener(
 // S,A,Wキーが押されたら速度を上げる
 window.addEventListener(
     "keyup",
-    event => { if (event.code == "KeyW" || event.code == "KeyS" || event.code == "KeyA") Runner.instance_.setSpeed(6) }
+    event => { if (event.code == "KeyW" || event.code == "KeyS" || event.code == "KeyA") Runner.instance_.setSpeed(mspeed) }
 );
 
 //ジャンプ機能
@@ -30,24 +36,30 @@ window.addEventListener(
 // Fキーが押されたら速度を上げる
 window.addEventListener(
     "keydown",
-    event => { if (event.code == "KeyF") Runner.instance_.tRex.config.GRAVITY = 0.2;
+    event => { if (event.code == "KeyF") Runner.instance_.tRex.config.GRAVITY = mgravity/3;
 }
 );
 
 // Eキーが押されたら速度を上げる
 window.addEventListener(
     "keydown",
-    event => { if (event.code == "KeyE") Runner.instance_.tRex.config.GRAVITY = 0.8;
+    event => { if (event.code == "KeyE") Runner.instance_.tRex.config.GRAVITY = mgravity/3*4;
  }
 );
 
 // E,Fキーが押されたら速度を上げる
 window.addEventListener(
     "keyup",
-    event => { if (event.code == "KeyE" || event.code == "KeyF") Runner.instance_.tRex.config.GRAVITY = 0.6 }
+    event => { if (event.code == "KeyE" || event.code == "KeyF") Runner.instance_.tRex.config.GRAVITY = mgravity }
 );
-function speed(s){Runner.instance_.setSpeed(s)}
-function gravity(s){Runner.instance_.tRex.config.GRAVITY = s}
+function speed(s){
+Runner.instance_.setSpeed(s)
+mspeed = s
+}
+function gravity(s){
+Runner.instance_.tRex.config.GRAVITY = s
+mgravity = s
+}
 /*
 © 2023 メタロロ
 */
